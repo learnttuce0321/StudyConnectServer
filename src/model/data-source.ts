52cac79dca5b861921f2636354a1eb4e56909aff
learnttuce0321 <lettucegood2@gmail.com>
2023-10-25 10:21:55 +0900

    보안보안

--- src/model/data-source.ts
+++ src/model/data-source.ts
@@ -1,15 +1,25 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from './MUser';
+import { Assignment } from "./MAssignment";
+import { Attendance } from "./MAttendance";
+import { AttendanceRate } from "./MAttendanceRate";
+import { Fine } from "./MFine";
+import { Message } from "./MMessage";
+import { Schedule } from "./MSchedule";
+import { Study } from "./MStudy";
+import { Submit } from "./MSubmit";
+import { SubmitRate } from "./MSubmitRate";
+import { info } from "src/config/config";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "127.0.0.1",
-    port: 3306,
-    username: "root",
-    password: "Juyee0415!",
-    database: "studyuser",
-    entities: [User],
+    host: info.host,
+    port: info.port,
+    username: info.username,
+    password: info.password,
+    database: info.database,
+    entities: [Assignment, Attendance, AttendanceRate, Fine, Message, Schedule, Study, Submit,SubmitRate, User],
     synchronize: true,
     logging: true,
     migrations: [],
