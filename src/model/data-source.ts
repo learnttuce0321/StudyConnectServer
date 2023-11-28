9e88759debe418d1622f7bda55e85d7f5c52dca6
learnttuce0321 <lettucegood2@gmail.com>
2023-11-28 19:18:40 +0900

    Fix: server setting

--- src/model/data-source.ts
+++ src/model/data-source.ts
@@ -1,27 +1,38 @@
 // import "reflect-metadata";
-import { DataSource } from "typeorm";
+import { DataSource } from 'typeorm';
 import { User } from './MUser';
-import { Assignment } from "./MAssignment";
-import { Attendance } from "./MAttendance";
-import { AttendanceRate } from "./MAttendanceRate";
-import { Fine } from "./MFine";
-import { Message } from "./MMessage";
-import { Schedule } from "./MSchedule";
-import { Study } from "./MStudy";
-import { Submit } from "./MSubmit";
-import { SubmitRate } from "./MSubmitRate";
-import { info } from "src/config/config";
+import { Assignment } from './MAssignment';
+import { Attendance } from './MAttendance';
+import { AttendanceRate } from './MAttendanceRate';
+import { Fine } from './MFine';
+import { Message } from './MMessage';
+import { Schedule } from './MSchedule';
+import { Study } from './MStudy';
+import { Submit } from './MSubmit';
+import { SubmitRate } from './MSubmitRate';
+import { info } from '../config/config';
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
+    type: 'mysql',
     host: info.host,
     port: info.port,
     username: info.username,
     password: info.password,
     database: info.database,
-    entities: [Assignment, Attendance, AttendanceRate, Fine, Message, Schedule, Study, Submit, SubmitRate, User],
+    entities: [
+        Assignment,
+        Attendance,
+        AttendanceRate,
+        Fine,
+        Message,
+        Schedule,
+        Study,
+        Submit,
+        SubmitRate,
+        User,
+    ],
     synchronize: true,
     logging: true,
     migrations: [],
-  subscribers: [],
-  });
+    subscribers: [],
+});
